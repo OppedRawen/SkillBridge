393ccaa017fdee1fd2c790fc3c1652c2ec971ab7
OppedRawen <yud2372@gmail.com>
2025-01-11 00:32:32 -0800

    model trained

--- Backend/data/test.py
+++ Backend/data/test.py
@@ -1,14 +1,3 @@
-import spacy
-
-# Load the pipeline
-nlp = spacy.load("model_output/model-best")
-
-# Some test text
-text = "We're looking for a developer with experience in iOS development and Docker."
-
-# Process the text
-doc = nlp(text)
-
-# Print recognized entities
-for ent in doc.ents:
-    print(ent.text, ent.label_)
+import cupy as cp
+print("CUDA Devices Available:", cp.cuda.runtime.getDeviceCount())
+print("Current Device ID:", cp.cuda.runtime.getDevice())
